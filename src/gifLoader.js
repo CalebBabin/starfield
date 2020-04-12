65ffbbc074a951c39a1069e70806f2dfd9e134d8
Caleb Babin <object.caleb@gmail.com>
2020-04-11 21:28:36 -0500

    initial commit

---
+++ src/gifLoader.js
@@ -0,0 +1,120 @@
+import * as THREE from 'three';
+
+class GIF_Instance {
+	constructor(id) {
+		this.id = id;
+		this.gifTiming = 10;
+		this.lastFrame = Date.now();
+		this.currentFrame = 0;
+		this.loadedImages = 0;
+		this.frames = [];
+
+		if (id.match(/http/)) {
+			this.url = id;
+			this.imageFallback();
+		} else {
+			fetch(`https://gif-emotes.opl.io/gif/${id}`)
+				.then(r => r.json())
+				.then(data => {
+					if (data.count === 0 || !data.count) {
+						this.url = `https://gif-emotes.opl.io/gif/${id}.gif`
+						this.imageFallback();
+					} else {
+						this.gifTiming = data.frames[0].delay;
+						this.frames = data.frames;
+						for (let index = 0; index < data.frames.length; index++) {
+							const frame = data.frames[index];
+							frame.image = new Image(frame.width, frame.height);
+							frame.image.crossOrigin = "anonymous";
+							frame.image.addEventListener('load', () => {
+								this.loadedImages++;
+							})
+							frame.image.src = `https://gif-emotes.opl.io/static/${id}/${index}.png`;
+						}
+						this.loadListener();
+					}
+				})
+		}
+
+
+
+
+		this.canvas = document.createElement('canvas');
+		this.canvas.width = 128;
+		this.canvas.height = 128;
+		this.ctx = this.canvas.getContext('2d');
+
+		this.texture = new THREE.CanvasTexture(this.canvas);
+		//this.rot_texture = new THREE.CanvasTexture(this.canvas);
+		//this.rot_texture.rotation = Math.PI/2;
+		this.material = new THREE.MeshBasicMaterial({ map: this.texture, transparent: true });
+		//this.rot_material = new THREE.MeshBasicMaterial({ map: this.rot_texture, transparent: true/*, rotation: Math.PI / 2*/ });
+	}
+
+	imageFallback() {
+		this.image = new Image();
+		this.image.crossOrigin = "anonymous";
+		this.image.addEventListener('load', this.imageFallbackListener.bind(this));
+
+		this.image.src = this.url;
+	}
+	imageFallbackListener() {
+		let pow = 2;
+		while (Math.pow(2, pow) < Math.max(this.image.width, this.image.height)) {
+			pow++;
+		}
+		this.canvas.width = Math.pow(2, pow);
+		this.canvas.height = Math.pow(2, pow);
+
+		const ratio = Math.min(this.canvas.height / this.image.height, this.canvas.width / this.image.width);
+
+		this.ctx.drawImage(this.image, 0, 0, this.image.width * ratio, this.image.height * ratio);
+		this.texture.needsUpdate = true;
+	}
+
+	loadListener() {
+		let pow = 2;
+		while (Math.pow(2, pow) < Math.max(this.frames[0].width, this.frames[0].height)) {
+			pow++;
+		}
+		this.canvas.width = Math.pow(2, pow);
+		this.canvas.height = Math.pow(2, pow);
+
+		/*const fx = this.frames[0].width;
+		const fy = this.frames[0].height;
+		if (fx >= fy) {
+			this.drawRatio = this.canvas.width / fx;
+		} else {
+			this.drawRatio = this.canvas.width / fy;
+		}*/
+
+		this.drawOffsetY = this.canvas.height - this.frames[0].height;
+		this.update();
+	}
+
+	update() {
+		window.setTimeout(this.update.bind(this), this.gifTiming * 10);
+
+		let timeDiff = Date.now() - this.lastFrame;
+		while (timeDiff > this.gifTiming * 10) {
+			this.currentFrame++;
+			if (this.currentFrame >= this.frames.length) this.currentFrame = 0;
+			timeDiff -= this.gifTiming;
+			this.lastFrame += timeDiff;
+
+			this.disposalMethod = this.frames[this.currentFrame].disposal;
+			if (this.frames[this.currentFrame].image.complete) {
+				if (this.disposalMethod === 2) {
+					this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+				}
+				this.ctx.drawImage(
+					this.frames[this.currentFrame].image,
+					this.frames[this.currentFrame].x,
+					this.frames[this.currentFrame].y + this.drawOffsetY);
+				this.texture.needsUpdate = true;
+			}
+		}
+	}
+}
+
+module.exports = GIF_Instance;
---
+++ src/main.js
@@ -0,0 +1,130 @@
+import * as THREE from 'three';
+
+const chatIntegration = require('./chat.js');
+
+const globalConfig = {
+	speed: 0.01,
+	emoteScale: 3,
+	starScale: 0.075,
+	spawnAreaSize: 50,
+	safeSpace: 3,
+	spawnRate: 1,
+	cameraDistance: 100,
+	cameraFar: 1000,
+}
+
+const getSpawnPosition = () => {
+	return (Math.random() > 0.5 ? -1 : 1) * (globalConfig.spawnAreaSize * Math.random() / 2) + globalConfig.safeSpace;
+}
+
+const plane_geometry = new THREE.PlaneBufferGeometry(globalConfig.emoteScale, globalConfig.emoteScale);
+const star_geometry = new THREE.SphereBufferGeometry(globalConfig.starScale);
+const star_material = new THREE.MeshBasicMaterial({ color: 0xffffff });
+
+window.addEventListener('DOMContentLoaded', () => {
+	let camera, scene, renderer;
+	const stars = [];
+	let spawnTick = 0;
+
+	init();
+	draw();
+
+	function init() {
+		camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.1, globalConfig.cameraFar);
+		camera.position.x = 0;
+		camera.position.y = 0;
+		camera.position.z = globalConfig.cameraDistance;
+		camera.lookAt(0, 0, 0);
+
+		scene = new THREE.Scene();
+
+		const light = new THREE.AmbientLight(0xeeeeee);
+		scene.add(light);
+
+		renderer = new THREE.WebGLRenderer({ antialias: true });
+		renderer.setSize(window.innerWidth, window.innerHeight);
+		window.addEventListener('resize', () => {
+			camera.aspect = window.innerWidth / window.innerHeight;
+			camera.updateProjectionMatrix();
+			renderer.setSize(window.innerWidth, window.innerHeight);
+		})
+		document.body.appendChild(renderer.domElement);
+	}
+
+	function draw() {
+		requestAnimationFrame(draw);
+
+		spawnTick += globalConfig.spawnRate;
+
+		while (spawnTick >= 1) {
+			spawnTick -= 1;
+
+			const mesh = new THREE.Mesh(star_geometry, star_material);
+			mesh.position.x = getSpawnPosition();
+			mesh.position.y = getSpawnPosition();
+
+
+			stars.push({
+				progress: 0,
+				mesh: mesh,
+			})
+
+			scene.add(mesh);
+			//spawn "star"
+		}
+
+		for (let index = stars.length - 1; index >= 0; index--) {
+			const star = stars[index];
+			star.progress += globalConfig.speed;
+
+			star.mesh.position.z = star.progress * globalConfig.cameraDistance;
+
+			if (star.progress >= 1) {
+				scene.remove(star.mesh);
+				stars.splice(index, 1);
+			}
+		}
+
+		for (let index = 0; index < chatIntegration.emotes.length; index++) {
+			const emotes = chatIntegration.emotes[index];
+
+			if (!emotes.group) {
+				emotes.group = new THREE.Group();
+				emotes.group.position.x = getSpawnPosition();
+				emotes.group.position.y = getSpawnPosition();
+				emotes.initGroup = true;
+			}
+
+			emotes.group.position.z = emotes.progress * globalConfig.cameraDistance;
+
+			if (emotes.progress > 1) {
+				for (let i = 0; i < emotes.emotes.length; i++) {
+					const emote = emotes.emotes[i];
+					emotes.group.remove(emote.sprite);
+				}
+				scene.remove(emotes.group);
+				chatIntegration.emotes.splice(index, 1);
+			} else {
+				emotes.progress += globalConfig.speed;
+
+				for (let i = 0; i < emotes.emotes.length; i++) {
+					const emote = emotes.emotes[i];
+					if (emote) {
+						if (!emote.sprite) {
+							emote.sprite = new THREE.Mesh(plane_geometry, new THREE.MeshBasicMaterial({ map: emote.material.texture, transparent: true }));
+							emote.sprite.position.x += i * globalConfig.emoteScale;
+							emotes.group.add(emote.sprite);
+						}
+					}
+				}
+
+				if (emotes.initGroup) {
+					emotes.initGroup = false;
+					scene.add(emotes.group);
+				}
+			}
+		}
+
+		renderer.render(scene, camera);
+	}
+})
